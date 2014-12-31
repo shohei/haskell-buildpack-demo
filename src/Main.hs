65de90fdf61f947aab04a5c7f5eedd6d1831c015
Shohei Aoki <shoaok@gmail.com>
2014-12-31 21:14:09 +0900

    Change to scotty

--- src/Main.hs
+++ src/Main.hs
@@ -1,14 +1,28 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import System.Environment
-import Network.HTTP.Types (status200)
-import Network.Wai
-import Network.Wai.Handler.Warp (run)
+import Web.Scotty
+import           Control.Applicative         ((<$>))
+import           System.Environment          (getEnv)
 
-application _ = return $
-  responseLBS status200 [("Content-Type", "text/plain")] "Hello world.\n\nThis is Haskell on Heroku.\nhttps://github.com/pufuwozu/haskell-buildpack-demo"
+import Data.Monoid (mconcat)
 
 main = do
-  port <- getEnv "PORT"
-  run (fromIntegral $ read port) application
+    port <- read <$> getEnv "PORT"
+    scotty port $ do
+        get "/:word" $ do
+            beam <- param "word"
+            html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+
+
+-- import System.Environment
+-- import Network.HTTP.Types (status200)
+-- import Network.Wai
+-- import Network.Wai.Handler.Warp (run)
+--
+-- application _ = return $
+--   responseLBS status200 [("Content-Type", "text/plain")] "Hello world.\n\nThis is Haskell on Heroku.\nhttps://github.com/pufuwozu/haskell-buildpack-demo"
+--
+-- main = do
+--   port <- getEnv "PORT"
+--   run (fromIntegral $ read port) application
